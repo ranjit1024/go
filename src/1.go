e5f0ba21bf40153fb722c2c581e71bf59f301a4f
ranjit1024 <ranjitdas2048@gmail.com>
2026-03-27 20:24:19 +0530

    new folder strucre

--- src/1.go
+++ src/1.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hellow world")
-	name := "ranjit"
-	fmt.Println(name)
-	for i := 1; i < 10; i++ {
-		fmt.Println(i)
-	}
+	fmt.Println("hellow World")
 }
