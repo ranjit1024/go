325032cece7ba5e5c6440f06bd07758f95312695
ranjit1024 <ranjitdas2048@gmail.com>
2026-03-28 22:06:20 +0530

    adding anoter file

--- src/second.go
+++ src/second.go
@@ -3,13 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("")
-	var a [10]int
-	for i := 0; i < 10; i++ {
-		a[i] = i * 2
-	}
-	b := a[:]
-	b = append(b, 222, 3434)
-	fmt.Println(b)
-	fmt.Println(a)
+	fmt.Print("data")
+	var s []string
+	s[0] = "ranjit"
+	fmt.Println(s)
 }
---
+++ src/third.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("data")
+	a := [4]int{1, 2, 3, 4}
+	b := a[:]
+	b = append(b, 12)
+	fmt.Println(b)
+}
