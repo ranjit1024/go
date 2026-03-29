7c8bd34bcc5fa208e1a58d1c7d0a2b1c83c8a33e
ranjit1024 <ranjitdas2048@gmail.com>
2026-03-29 15:53:45 +0530

    adding file

--- src/func.go
+++ src/func.go
@@ -1,17 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func plus(a int, b int) int {
-	if a == 12 && b == 12 {
-		return a * b
-	}
+func add(a int, b int) int {
 	return a + b
 }
-
+func constant() (float64, float64) {
+	return 32.14, 1.18
+}
 func main() {
-	res := plus(12, 12)
-
-	fmt.Println(res)
-	fmt.Println("fad")
+	fmt.Println("data this is data we are talking about this is main datae ")
+	a := add(12, 12)
+	_, dat := constant()
+	fmt.Println(dat)
+	fmt.Println(a)
 }
