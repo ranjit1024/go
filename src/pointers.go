68dcc6dacc5a8c7b4b68281e645133b0059b73e4
ranjit1024 <ranjitdas2048@gmail.com>
2026-03-30 22:45:59 +0530

    added string'

--- src/pointers.go
+++ src/pointers.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-func zero_val() {
-
+func zero_val(ival int) {
+	ival = 0
 }
-
+func zero_valPointer(inter *int) {
+	*inter = 0
+}
 func main() {
 	fmt.Println("data")
+	i := 1
+	zero_val(i)
+	zero_valPointer(&i)
 }
---
+++ src/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Data")
+	const name = "ranjit"
+	fmt.Println(name)
+	fmt.Println("this is data")
+}
