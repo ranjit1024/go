7065614fd8994edfc6b7459ce4e2c0b5ef82e301
ranjit1024 <ranjitdas2048@gmail.com>
2026-04-01 19:48:40 +0530

    adding string

--- src/string.go
+++ src/string.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+func data() {
+	fmt.Println("this is data we are")
+}
 func main() {
+	data()
 	fmt.Println("Data")
 	const name = "ranjit"
 	fmt.Println(name)
 	fmt.Println("this is data")
+	data := "data"
+	fmt.Println(data)
 }
