1d6c8dbda136be4b227c1e0d2c8f7afdccb18c50
ranjit1024 <ranjitdas2048@gmail.com>
2026-04-05 14:29:38 +0530

    added some thing

--- src/range.go
+++ src/range.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Data")
 	nums := []int{10, 20, 30, 40}
+
 	for _, val := range nums {
 		fmt.Println(val)
 	}
--- src/third.go
+++ src/third.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	fmt.Println("data")
 	a := [4]int{1, 2, 3, 4}
+	len(a)
 	b := a[:]
 	b = append(b, 12)
 	fmt.Println(b)
