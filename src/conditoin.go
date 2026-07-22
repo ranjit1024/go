b9d9fb8da791a64189a872c4d673b5ffa1a47509
ranjit1024 <ranjitdas2048@gmail.com>
2026-07-22 23:17:28 +0530

    add:added conditoin

---
+++ src/conditoin.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World");
+	if 7 % 2 == 0{
+		fmt.Println("not a even")
+	}else{
+		fmt.Println("not a even number")
+	}
+	
+}
--- src/loop.go
+++ src/loop.go
@@ -1,10 +1,9 @@
 package main
-
 import "fmt"
 
 func main(){
 	for i := range 3 {
 		fmt.Println(i);
-		i++;
+		i++
 	}
 }
