f63b028d24e0596e9fb282ca025ab8229e326e78
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-09 12:06:06 +0530

    update:added array thing

--- src/Array.go
+++ src/Array.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Talking about the Array")
+	var a [5]int
+	fmt.Println(a)
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(b)
+
+	b = [...]int{12, 12, 12, 34, 34}
+	fmt.Println(b)
 
 }
