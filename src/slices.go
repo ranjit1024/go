4fdc57c8de82e94485bbfa3e0d0e01851e959f69
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-10 20:31:55 +0530

    update: added slice

--- src/slices.go
+++ src/slices.go
@@ -5,6 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("Data")
 	a := [5]int{1, 2, 3, 4, 5}
-	b := a
+	b := a[1:3]
+
+	fmt.Println(b)
+	fmt.Println(cap(b))
+
+	b = append(b, 12)
+	b = append(b, 34)
+
+	fmt.Println(b)
+
+	b = append(b, 100)
 	fmt.Println(b)
 }
