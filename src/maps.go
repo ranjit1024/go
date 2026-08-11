e54036a2dc72adf73bd7f50b140a152fd1c75bb2
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-11 21:25:29 +0530

    udpate

--- src/maps.go
+++ src/maps.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Data is the oil")
 	m := make(map[int]string)
+	marks := make([]int, 4, 10)
+	fmt.Println(marks)
 	m[1] = "ranjit"
 	fmt.Println(m)
+	clear(m)
 	a := 12
 	fmt.Println(a)
 }
