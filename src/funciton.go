721b22a1378f3ee0d3269cba8b05744166276e45
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-11 21:37:05 +0530

    almost done with funciton

---
+++ src/funciton.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(a int, b int) int {
+	return a + b
+}
+func main() {
+	a := [4]int{1, 2, 3, 4}
+	s := make([]int, 2, 4)
+	fmt.Println(a)
+	fmt.Println(s)
+	fmt.Println("Data is the king")
+	res := sum(3, 6)
+	fmt.Println(res)
+}
