ee109d5a83050a615dc4913df645ab2e68c1dfa6
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-13 22:26:19 +0530

    learn:closure

---
+++ src/closure.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 12
+	return func() int {
+		i++
+		return i
+	}
+
+}
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+}
