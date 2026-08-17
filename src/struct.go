c56869a079b30b19ef0f4e3dcf780168a4031581
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-17 21:51:33 +0530

    update:added struct

---
+++ src/struct.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name    string
+	roll_no int
+}
+
+func main() {
+	fmt.Println("Data is the king")
+	p1 := person{name: "Ranjit", roll_no: 12}
+	fmt.Println(p1)
+
+}
