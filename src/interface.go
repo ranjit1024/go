019e7567c0ee72bf238437cbdc3ef98b8aad6871
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-22 23:03:44 +0530

    understand  interface

--- src/interface.go
+++ src/interface.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type geometry interface {
+	area() int
+}
+type rect struct {
+	height, widht int
+}
+
+func (r rect) area() int {
+	return r.height * r.widht
+}
 func main() {
+	sqare := rect{
+		height: 12,
+		widht:  12,
+	}
+
+	var g geometry = sqare
+	fmt.Println(g.area())
+
 	fmt.Println("Data is the oil")
 }
