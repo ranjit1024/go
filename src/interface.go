4756d37ef69a201ca1593eeae0a1a0a41ae675e5
ranjit1024 <ranjitdas2048@gmail.com>
2026-08-22 22:49:55 +0530

    udpate:

--- src/interface.go
+++ src/interface.go
@@ -2,29 +2,6 @@ package main
 
 import "fmt"
 
-type geometry interface {
-	area() float32
-	para() float32
-}
-type rect struct {
-	width, height int
-}
-
-func (r rect) area() float32 {
-	return float32(r.width * r.height)
-}
-
-func (r rect) para() float32 {
-	return float32(r.height + r.width)
-}
-
-func measure(g geometry) {
-	fmt.Println(g)
-	fmt.Println(g.area())
-	fmt.Println(g.para())
-}
-
 func main() {
-
-	fmt.Println("DAta is the king")
+	fmt.Println("Data is the oil")
 }
