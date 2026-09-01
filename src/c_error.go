ef15f5a74bea27ced55b472e34cf74a5e86eb9cc
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-01 22:05:47 +0530

    done

--- src/c_error.go
+++ src/c_error.go
@@ -10,8 +10,16 @@ type argError struct {
 func (e *argError) Error() string {
 	return fmt.Sprintf("%d - %s", e.arg, e.message)
 }
+func f(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg: arg, message: "Can't work with it"}
+	}
+	return arg + 3, nil
+}
 func main() {
 	var a int
+	_, err := f(42)
+	fmt.Println(err)
 	fmt.Scanf("%d", &a)
 	fmt.Println(a)
 
