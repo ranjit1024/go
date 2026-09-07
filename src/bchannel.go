b9448b2816a6308ed32f5e028a2ab00fbe990046
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-07 22:23:26 +0530

    udpate

--- src/bchannel.go
+++ src/bchannel.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Data is the king")
-	stage1 := make(chan int, 3)
-	stage1 <- 12
+	stage1 := make(chan int)
+
+	for i := 1; i <= 10; i++ {
+		stage1 <- i
+	}
+
 	for i := 1; i <= 10; i++ {
 		fmt.Println(<-stage1)
 	}
