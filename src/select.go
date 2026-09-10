e32444b4fb4a99009d010b2475d193537842a11e
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-10 20:14:18 +0530

    udpate:added select

--- src/select.go
+++ src/select.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		c1 <- "one"
+
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "Two"
+	}()
+
+	for range 2 {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("recived", msg2)
+		}
+	}
 	fmt.Println("Data is the oil")
 }
