8c6038045497dc0a06f7f95dbdd2a4309c024ef0
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-15 19:17:27 +0530

    udpate:added range over channel

---
+++ src/Concepts/range_over_chnnel.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	queue := make(chan string, 2)
+	queue <- "Ranjit"
+	queue <- "Sammmu"
+	close(queue)
+
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+	fmt.Println("Data is the oil")
+}
