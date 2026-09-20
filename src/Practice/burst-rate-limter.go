12af8d47e7b7cdf040a3c597f3288ffd10760b58
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-20 14:47:23 +0530

    addedL:brust job proceser

--- src/Practice/burst-rate-limter.go
+++ src/Practice/burst-rate-limter.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Dta is the oil")
+	jobs := make(chan int, 10)
+	for i := range 10 {
+		jobs <- i
+	}
+	close(jobs)
+
+	b_rate_limimter := make(chan time.Time, 3)
+
+	for range 3 {
+		b_rate_limimter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(time.Second) {
+			b_rate_limimter <- t
+		}
+	}()
+
+	for job := range jobs {
+		<-b_rate_limimter
+		fmt.Println("Processing job", job, "at", time.Now())
+	}
+
 }
