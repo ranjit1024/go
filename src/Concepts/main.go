141ae632421398de06fcbba7cd14af0b7f865f80
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-20 19:33:20 +0530

    update:done

--- src/Concepts/main.go
+++ src/Concepts/main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("--------------Start--------------------")
-	demo_automic_counter()
+	demo_mutex()
 	fmt.Println("--------------Main go End-----------------")
 }
