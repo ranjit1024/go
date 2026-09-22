d7c0f8ac56f72baf4e03d3ddbb21c5fc28a9a1a5
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-22 22:11:13 +0530

    update

--- src/Practice/I.go
+++ src/Practice/I.go
@@ -1,10 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // / stuende grade analuzer
 func first() {
-	scores := []int{12, 56, 34, 23, 90, 99, 34, 23}
+	scores := []int{56, 99, 34, 23, 90, 34}
 	fmt.Println(scores)
+
+	for i := range len(scores) {
+		for j := i; j < len(scores); j++ {
+			if scores[i] > scores[j] {
+				fmt.Println(scores[i])
+			}
+		}
+	}
+	for i := range len(scores) {
+
+		for j := i; j < len(scores); j++ {
+			if scores[i] > scores[j] {
+				fmt.Println(scores[i])
+			}
+		}
+		fmt.Println()
+
+	}
+
 	fmt.Println("Data is the key")
 }
