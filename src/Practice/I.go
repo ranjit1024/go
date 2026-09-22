e501930b2afafdf8e4cb8b50f988e838ca778c8a
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-22 21:16:00 +0530

    update:added mod

---
+++ src/Practice/I.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// / stuende grade analuzer
+func first() {
+	scores := []int{12, 56, 34, 23, 90, 99, 34, 23}
+	fmt.Println(scores)
+	fmt.Println("Data is the key")
+}
--- src/Practice/first-excersice.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func first() {
-	fmt.Println("Data is the key")
-}
