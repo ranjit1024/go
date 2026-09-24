8976c90dbd28989e0cea2ac97fbd8b38da215263
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-24 20:55:44 +0530

    update:added practice

--- src/Practice/I.go
+++ src/Practice/I.go
@@ -7,20 +7,24 @@ import (
 // / stuende grade analuzer
 func first() {
 	scores := []int{56, 99, 34, 23, 90, 34}
-	
+
 	h_n := scores[0]
 	l_n := scores[0]
-
+	sum := 0
 	for i := 1; i < len(scores); i++ {
 		if h_n < scores[i] {
 			h_n = scores[i]
-			fmt.Println(h_n)
+
 		}
 		if l_n > scores[i] {
 			l_n = scores[i]
-			fmt.Println(l_n)
+
 		}
 	}
+	for i := range len(scores) {
+		sum = sum + scores[i]
+	}
+	fmt.Println(sum)
 
-	fmt.Println("Data is the key")
+	fmt.Println("Data is the key", h_n, l_n)
 }
