73cbc445c59fb57af4db5d985f923c0be5bd9b8b
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-24 22:34:37 +0530

    update:for sec

--- src/Practice/II.go
+++ src/Practice/II.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Account struct {
+	Id      int
+	Name    string
+	Balance int
+}
+
+func Deposite(amount int) {
+
+}
+
+func withDraw(amount int) {
+
+}
+
+func getBalance() {
+
+}
 
 func Second() {
-	fmt.Println("Data is the king")
+	fmt.Println("Data is the oil")
 }
