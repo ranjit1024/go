f9aa248823a5cbb6d41b1fc4b8f22fa7cbe9539b
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-26 14:44:05 +0530

    update:added anoter

--- src/Practice/II.go
+++ src/Practice/II.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-type Account struct {
-	Id      int
-	Name    string
-	Balance int
-}
-
-func Deposite(amount int) {
-
-}
-
-func withDraw(amount int) {
-
-}
-
-func getBalance() {
-
+func countWords(text string) map[string]int {
+	res := make(map[string]int)
+	words := strings.Split(text, "")
+	fmt.Println(words)
+	return res
 }
 
 func Second() {
+	text := "go is simple and go is powerful and go is fast"
+	fmt.Println(text)
+
 	fmt.Println("Data is the oil")
+
 }
