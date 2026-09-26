e924df06ae7cc427ed73748451332632ed5190e2
ranjit1024 <ranjitdas2048@gmail.com>
2026-09-26 15:27:55 +0530

    update:done

--- src/Practice/II.go
+++ src/Practice/II.go
@@ -7,15 +7,20 @@ import (
 
 func countWords(text string) map[string]int {
 	res := make(map[string]int)
-	words := strings.Split(text, "")
+	words := strings.Split(text, " ")
 	fmt.Println(words)
+	for i := range len(words) {
+		word := words[i]
+		res[word]++
+	}
+	fmt.Println(res)
 	return res
 }
 
 func Second() {
 	text := "go is simple and go is powerful and go is fast"
 	fmt.Println(text)
-
+	countWords(text)
 	fmt.Println("Data is the oil")
 
 }
